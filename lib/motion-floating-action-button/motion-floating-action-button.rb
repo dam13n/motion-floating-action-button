d96bf0d00ce4491a263256144a4a22f9b3548afd
Damien <damien@fusion.ucla.edu>
2015-11-26 00:06:20 -0800

    Button image: get UIImage from args

--- lib/motion-floating-action-button/motion-floating-action-button.rb
+++ lib/motion-floating-action-button/motion-floating-action-button.rb
@@ -4,11 +4,11 @@ module MotionFloatingActionButton
 
   def build(args)
 
-    puts args
-    $args = args
     @floating_frame = args[:floating_frame] || CGRectMake(device.width*3/4, device.height/2, device.width*1/4, device.height/2)
+    @normal_image = args[:normal_image]
+    @pressed_image = args[:pressed_image]
 
-    addButton = VCFloatingActionButton.alloc.initWithFrame(@floating_frame, normalImage: UIImage.imageNamed('icon-29'), andPressedImage: UIImage.imageNamed('icon-29'), withScrollview: UITableView.new)
+    addButton = VCFloatingActionButton.alloc.initWithFrame(@floating_frame, normalImage: @normal_image, andPressedImage: @pressed_image, withScrollview: UITableView.new)
 
     addButton
   end
